b7b74dac73e4ab78589f96236fda19c377e43163
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-02-11 12:55:30 +0100

    !!! TASK: Neos 8.2 compatibility

--- Resources/Private/Inspector/src/inspector.js
+++ Resources/Private/Inspector/src/inspector.js
@@ -1,24 +1,18 @@
-import { Plugin } from 'ckeditor5-exports';
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
 let currentlyFocusedInspectorName = null;
 
-export default class Inspector extends Plugin {
-    static get pluginName() {
-        return 'Inspector';
-    }
+export const InspectorPlugin = (editor) => {
 
-    init() {
-        // we only attach the inspector on focus; and we detach the last focused
-        // editor - so that the currently focused editor is the only inspector being shown.
-        this.editor.ui.focusTracker.on('change:isFocused', event => {
-            if (event.source.isFocused) {
-                if (currentlyFocusedInspectorName) {
-                    CKEditorInspector.detach(currentlyFocusedInspectorName);
-                }
-                currentlyFocusedInspectorName = `${this.editor.neos.contextPath}: ${this.editor.neos.propertyName}`;
-                CKEditorInspector.attach(currentlyFocusedInspectorName, this.editor);
+    // we only attach the inspector on focus; and we detach the last focused
+    // editor - so that the currently focused editor is the only inspector being shown.
+    editor.ui.focusTracker.on('change:isFocused', (event) => {
+        if (event.source.isFocused) {
+            if (currentlyFocusedInspectorName) {
+                CKEditorInspector.detach(currentlyFocusedInspectorName);
             }
-        });
-    }
+            currentlyFocusedInspectorName = `${editor.neos.contextPath}: ${editor.neos.propertyName}`;
+            CKEditorInspector.attach(currentlyFocusedInspectorName, editor);
+        }
+    });
 }
